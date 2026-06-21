c168244fc256c7dc5a692e627c76e6a1fc70044f
fedehinojosa95 <fedehinojosa95@gmail.com>
2026-06-20 18:46:18 -0600

    reset-pos.bat: borrar también %LOCALAPPDATA% (cache HTTP/Chromium)

--- public/reset-pos.bat
+++ public/reset-pos.bat
@@ -10,9 +10,12 @@ taskkill /F /IM "Modo.Tab POS.exe" 2>nul
 taskkill /F /IM "pos-app.exe" 2>nul
 timeout /t 2 /nobreak >nul
 
-echo Borrando configuracion local...
+echo Borrando configuracion local (AppData)...
 rmdir /s /q "%APPDATA%\Modo.Tab POS" 2>nul
 rmdir /s /q "%APPDATA%\pos-app" 2>nul
+echo Borrando cache local (LocalAppData)...
+rmdir /s /q "%LOCALAPPDATA%\Modo.Tab POS" 2>nul
+rmdir /s /q "%LOCALAPPDATA%\pos-app" 2>nul
 
 echo.
 echo ========================================
